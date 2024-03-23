a888aa3c6d891932d8884911201cf5987089846d
JSKLGNA <ajsklgn@gmail.com>
2024-03-23 15:05:57 +0300

    Initial Commit

---
+++ The bill1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The bill1
+//
+//  Created by JSKLJN on 23.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
